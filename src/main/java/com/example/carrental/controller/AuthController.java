09a0ae015631f56f1cc4457b58afb8c70ad3f6fc
GUBBALA HEMANTH SAI <123979945+hemanth0804@users.noreply.github.com>
2025-09-25 12:10:34 +0530

    Update AuthController.java

--- src/main/java/com/example/carrental/controller/AuthController.java
+++ src/main/java/com/example/carrental/controller/AuthController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 import com.example.carrental.model.JwtResponse;
 import com.example.carrental.model.LoginRequest;
@@ -25,6 +27,7 @@ import com.example.carrental.service.UserService;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173") 
 public class AuthController {
 
     @Autowired
